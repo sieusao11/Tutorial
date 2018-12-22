4a43a1f601a01e91c45e596adcbb775e61959a5a
HieuCR7 <hieuhv97@gmail.com>
2018-12-22 09:12:53 +0700

    chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    base_title = t "layouts.application.title"
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
